38482d65bb781c6cb1bf472d3b5505dc68781c10
syukur1 <72872356+syu-kuri@users.noreply.github.com>
2024-07-02 21:34:02 +0900

    feat: add show display info. (#1)

---
+++ client/client.lua
@@ -0,0 +1,16 @@
+RegisterNetEvent('displayInfo')
+AddEventHandler('displayInfo', function(time)
+    if Config.ShowServerName and Config.ServerName then
+        time = time .." | © " .. Config.ServerName
+    end
+    if Config.ShowPlayerName then
+        time = time .. "\n" .. Config.ServerName .. ": " .. GetPlayerName(NetworkGetEntityOwner(PlayerPedId()))
+    end
+    if Config.ShowPlayerID then
+        time = time .. "\n" .. Config.PlayerID .. ": " .. GetPlayerServerId(NetworkGetEntityOwner(PlayerPedId()))
+    end
+    SendNUIMessage({
+        action = "setDisplayInfo",
+        time = time
+    })
+end)
